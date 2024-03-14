5d73001ea75eef436a064cb55f2f06558b1819ee
Seif Sameh <seifsameh2626@gmail.com>
2024-03-14 17:26:49 +0200

    .

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { BrowserRouter } from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={Store}>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter >
         <App />
       </BrowserRouter>
     </Provider>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -47,6 +47,7 @@ function Home() {
       
     };
   },[user])
+
   
   return (
     <>
